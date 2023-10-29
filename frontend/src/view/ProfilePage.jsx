8e7d681e64f56d65899b2352add3fe5e738f1e5f
Aman Kumar Prasad <amankumarprasad72@gmail.com>
2023-10-29 23:56:56 +0530

    update

--- frontend/src/view/ProfilePage.jsx
+++ frontend/src/view/ProfilePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Profilepage = () => {
   return (
     <div>
-      
+      <h1>Profilepage</h1>
     </div>
   )
 }
--- frontend/src/view/loginPage.jsx
+++ frontend/src/view/loginPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const loginpage = () => {
   return (
     <div>
-      
+      <h1>loginpage</h1>
     </div>
   )
 }
